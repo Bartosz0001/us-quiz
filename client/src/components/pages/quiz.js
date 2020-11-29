5dcfdd812e05768a0569b519a16946d66e0cad1e
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-11-29 16:53:40 +0100

    Add timer

--- client/src/components/pages/quiz.js
+++ client/src/components/pages/quiz.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { API_URL } from '../../config';
 
 import './quiz.scss';
 
 const Quiz = () => {
     const [question, setQuestion] = useState({});
+    let [time, setTime] = useState(30);
+
     useEffect(() => {
         fetch(`${API_URL}/questions`).then(
             res => res.json()
@@ -13,13 +14,29 @@ const Quiz = () => {
             data => {
                 const rand = Math.floor(Math.random() * data.length);
                 setQuestion(data[rand]);
-                console.log(data[rand]);
+
+                const timer = setTimeout(() => {
+                    console.log('Time\'s up!');
+                }, 30000);
+
+                const countTime = setInterval(() => {
+                    if(time > 0) {
+                        time--;
+                        setTime(time);
+                    }
+                }, 1000);
+
+                return () => {
+                    clearTimeout(timer);
+                    clearInterval(countTime);
+                }
             }
         )
     }, []);
 
     return (
         <div className='container'>
+          <div className='timer'><h2 className='time'>{time}</h2></div>
           <div className='question'><h1 className='question-text'>{question.questionText}</h1></div>
           <div className='question-map'><img src={question.questionMap} alt='map' /></div>
           <div className='answers'>
